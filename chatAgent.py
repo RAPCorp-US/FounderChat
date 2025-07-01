70a14bb65c0c6045ea7ac000925fbd344521ce28
RAPCorp-US <163478179+RAPCorp-US@users.noreply.github.com>
2025-06-30 23:28:14 -0500

    Update chatAgent.py

--- chatAgent.py
+++ chatAgent.py
@@ -6,7 +6,6 @@ import joblib
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.vectorstores import FAISS
 import langchain.docstore.document as docstore_document
-import langchain.docstore.document as docstore_document
 
 # This patch is a temporary workaround for the loading error.
 # It should be removed once the vector store is recreated.
